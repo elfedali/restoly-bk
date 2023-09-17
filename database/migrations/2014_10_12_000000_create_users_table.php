3be0c80f0d0d0bd7bb33510b8e78e3454f425527
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-09-17 18:19:02 +0100

    Install laravel breeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->string('phone', 100)->nullable();
+            $table->string('avatar')->nullable();
+
+            $table->string('name', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
